77e1c43b3738edbea146a6395970e990b3c46cfc
frectonz <fraol0912@gmail.com>
2026-05-14 00:20:43 +0300

    chore: big bang

---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      forAllSystems =
+        fn:
+        let
+          systems = [
+            "x86_64-linux"
+            "aarch64-darwin"
+          ];
+          overlays = [ (import rust-overlay) ];
+        in
+        nixpkgs.lib.genAttrs systems (
+          system:
+          fn (
+            import nixpkgs {
+              inherit system overlays;
+            }
+          )
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs:
+      {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.bacon
+            pkgs.cargo-pgrx
+            pkgs.cargo-insta
+            pkgs.rust-analyzer
+            pkgs.rust-bin.stable.latest.default
+          ];
+
+          inputsFrom = with pkgs; [
+            postgresql_14
+            postgresql_15
+            postgresql_16
+            postgresql_17
+            postgresql_18
+          ];
+
+          nativeBuildInputs = [
+            pkgs.rustPlatform.bindgenHook
+          ];
+        };
+      });
+
+      formatter = forAllSystems (
+        pkgs:
+        pkgs.treefmt.withConfig {
+          runtimeInputs = [ pkgs.nixfmt ];
+          settings = {
+            on-unmatched = "info";
+            formatter.nixfmt = {
+              command = "nixfmt";
+              includes = [ "*.nix" ];
+            };
+          };
+        }
+      );
+    };
+}
